fdb57cbef156d6ccf24fd133109f3443ac114909
fsousa <fsousabt@gmail.com>
2025-05-10 17:40:47 -0300

    adicionando banco sqlite

--- pdfmanager/core/build.gradle.kts
+++ pdfmanager/core/build.gradle.kts
@@ -3,5 +3,7 @@ plugins {
 }
 
 dependencies {
-    // Depedencias de core 
+    implementation("org.xerial:sqlite-jdbc:3.45.1.0")
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
 }
